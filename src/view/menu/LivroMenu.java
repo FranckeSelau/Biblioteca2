e79df182fdc925ffb8c5bd81181ae63d3d544697
FranckeSelau <francke.selau@gmail.com>
2016-05-01 21:47:23 -0300

    insersão de documentação Javadocs menu

--- src/view/menu/LivroMenu.java
+++ src/view/menu/LivroMenu.java
@@ -1,11 +1,22 @@
 
 package view.menu;
 
+/**
+ * Classe para interação Menu de Livros
+ *
+ * @author Francke
+ * @since JDK 1.0
+ */
 public class LivroMenu {
     public static final int OP_CADASTRAR = 1;
     public static final int OP_LISTAR = 2;
     public static final int OP_VOLTAR = 0;
 
+    /**
+     * Retorna as opções do Menu Livro.
+     *
+     * @return opções para o menu cadastro Livros
+     */
     public static String getOpcoes() {
         return ("\n--------------------------------------\n"
                 + "1 - Cadastrar Livros\n"
